655eefb1e4c0e182e08ca6ecede1c544cc1013aa
José Maria <jose.souza@hotmail.com>
2024-12-15 12:38:26 -0300

    Addes first and last name

--- ConsoleApp.UserInputDemo/Program.cs
+++ ConsoleApp.UserInputDemo/Program.cs
@@ -1,11 +1,15 @@
 ﻿// Declare variables
-string name = string.Empty;
+string firstName = string.Empty;
+string lastName = string.Empty;
 int age = 0;
 int retirementAge = 65;
 
 // Prompt the for input
-Console.WriteLine("Please anter your name");
-name = Console.ReadLine();
+Console.WriteLine("Please anter your first name");
+firstName = Console.ReadLine();
+
+Console.WriteLine("Please anter your last name");
+lastName = Console.ReadLine();
 
 // Process the data
 Console.WriteLine("Please anter your age");
@@ -13,6 +17,6 @@ age = Convert.ToInt32(Console.ReadLine());
 int workingYearsRemaining = retirementAge - age;
 
 // Output the results to the user
-Console.WriteLine($"Full name: {name}");
+Console.WriteLine($"Full name: {firstName} {lastName}");
 Console.WriteLine($"Age: {age}");
 Console.WriteLine($"Working yares remaining: {workingYearsRemaining}");
